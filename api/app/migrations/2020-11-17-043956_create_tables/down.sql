160e3dffefb4ef58ce51460fb5bbe40c453d5c5d
Brian Hotopp <brihoto@gmail.com>
2021-04-21 21:10:00 -0400

    added django-rest framework

--- api/app/migrations/2020-11-17-043956_create_tables/down.sql
+++
@@ -1,8 +0,0 @@
--- drop table cascade drops dependent objects
--- in this case it just means it drops the foreign key constraint of the dependent tables
-DROP VIEW menu_entries_complete
-DROP TABLE users CASCADE;
-DROP TABLE menuentries CASCADE;
-DROP TABLE food_ratings;
-DROP TABLE restaurants CASCADE;
-DROP TABLE trust_ratings CASCADE;
---
+++ hard-reset.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo docker rm -f $(sudo docker ps -a -q)
+sudo docker volume rm $(sudo docker volume ls -q)
