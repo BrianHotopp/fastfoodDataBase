37f36b56fdbae39de18be3a0254bb2e9aec75b6e
seanpoh1 <pohe@rpi.edu>
2021-04-25 17:40:55 -0400

    revamped profile page

--- frontend/fastfood_frontend/src/App.vue
+++ frontend/fastfood_frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="lg:px-64 flex flex-col h-screen text-myblack bg-bgcolor">
+  <div id="app" class="lg:px-64 flex flex-col min-h-screen text-myblack bg-bgcolor">
     <header>
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;900&display=swap" rel="stylesheet">
--- frontend/fastfood_frontend/src/views/Profile.vue
+++ frontend/fastfood_frontend/src/views/Profile.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <div>
-      <img :src="$auth.user.picture">
-      <h2>{{ $auth.user.name }}</h2>
-      <p>{{ $auth.user.email }}</p>
-    </div>
-
-    <div>
-      <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+    <div class='px-5 py-5'>
+      <div class='bg-bgcolor+1 px-5 py-5 m-1 rounded-md'>
+        <img :src="$auth.user.picture" class='border border-myorange'>
+        <br>
+        <p class='font-buttonfont font-semibold text-myorange text-xl'>Name</p>
+        <p class='font-buttonfont text-white'>{{ $auth.user.name }}</p>
+        <br>
+        <p class='font-buttonfont font-semibold text-myorange text-xl'>Email</p>
+        <p class='font-buttonfont text-white'>{{ $auth.user.email }}</p>
+        <br>
+        <p class='font-buttonfont font-semibold text-myorange text-xl'>Nickname</p>
+        <p class='font-buttonfont text-white'>{{ $auth.user.nickname }}</p>
+        <br>
+        <p class='font-buttonfont font-semibold text-myorange text-xl'>Raw Data</p>
+        <div class='bg-bgcolor text-white rounded-md'>
+        <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+        </div>
+      </div>
     </div>
   </div>
 </template>
